fafca7d0d997f87f07470821119d3ddf45956455
Milan Pavlik <pavlik.mil@gmail.com>
2016-09-18 23:58:54 +0100

    Generate initial grid.

--- core/game/reducers.js
+++ core/game/reducers.js
@@ -1,3 +1,14 @@
+const range = (n) => Array.apply(null, { length: n }).map(Number.call, Number);
+const generateCell = () => ({ mine: false, flagged: false, neighbours: 0 });
+const generateRow = (n) => range(n).map(generateCell);
+const generateGrid = (n) => range(n).map(generateRow.bind(null, n));
+
+
+const initialState = {
+  lost: false,
+  won: false,
+  grid: generateGrid(10)
+};
 
 
 const game = (state = {}, action) => {
