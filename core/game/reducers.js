67b8657fba0bcc634a6fcc6d07b3687515044ad4
Milan Pavlik <pavlik.mil@gmail.com>
2016-09-20 23:58:42 +0100

    Start gridsearch algorithm.

--- core/game/reducers.js
+++ core/game/reducers.js
@@ -4,6 +4,24 @@ const generateRow = (n) => range(n).map(generateCell);
 const generateGrid = (n) => range(n).map(generateRow.bind(null, n));
 
 
+const closeNeighbours = (grid, row, col) => {
+  const width = grid.length;
+  const length = grid[0].length;
+
+  const rowMin = Math.max(0, row - 1);
+  const rowMax = Math.min(width, row + 1);
+  const colMin = Math.max(0, col - 1);
+  const colMax = Math.min(length, col + 1);
+
+  const rows = grid.slice(rowMin, rowMax + 1);
+  return rows
+    .map(row => slice(colMin, colMax + 1))
+  return rows.;
+};
+const neighbours = (grid) => {
+  grid
+}
+
 
 const initialState = {
   lost: false,
